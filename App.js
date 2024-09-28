0f3ae78bac15db65c11ec086617c98502531c527
Humberto Queiroz <90359980+HumbertoQueiroz@users.noreply.github.com>
2024-09-28 15:44:34 -0400

    Nova pagina inicial

--- App.js
+++ App.js
@@ -1,32 +1,78 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Modal } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
 
 export default function App() {
   const [pageLogin,setPageLogin]=useState(false)
   const [pageCadastro,setpageCadastro]=useState(false)
+  const [userEmail, setUserEmail]=useState('')
+  const [errorEmail,seterrorEmail]=useState(false)
+  const [errorPasseord,seterrorPasseord]=useState(false)
+
+  function onChangeEmail(){
+
+  }
 
   return (
     <View style={styles.containerMain}>
-      <View style={styles.flexRow}>
+      <View style={[styles.flexRow,{marginBottom:75}]}>
         <Image source={require("./src/assets/Logo400x400.png")} style={styles.logoMain} alt='Logo do Corpo de bombeiros Militar de Mato Grosso' />
-        <Text style={styles.textMain}>Emergências</Text>
-        <Text style={styles.textMain}>193</Text>
+        <Text style={styles.textMain}>
+          Emergências
+        </Text>
+        <Text style={styles.textMain}>
+          193
+        </Text>
       </View>
-      <Text style={styles.textBase}>Efetue seu Login</Text>
+      <Text style={[styles.textBase,{marginBottom:32}]}>
+        Efetue seu Login
+      </Text>
       <View style={styles.flexRow}>
         <TouchableOpacity onPress={()=>{}} style={styles.buttonSemiRounded}>
-          <Image source={require('./src/assets/iconFacebook.png')}  style={styles.icon}/>
+          <View style={styles.containerIcon}>
+            <Image source={require('./src/assets/iconFacebook.png')}  style={styles.icon}/>
+          </View>
         </TouchableOpacity>
         <TouchableOpacity onPress={()=>{}} style={styles.buttonSemiRounded}>
-          <Image source={require('./src/assets/iconGoogle.png')}  style={styles.icon}/>
+          <View style={styles.containerIcon}>
+            <Image source={require('./src/assets/iconGoogle.png')}  style={styles.icon}/>
+          </View>
         </TouchableOpacity>
       </View>
-      <TouchableOpacity onPress={()=>{}} style={styles.button}>
-        <Text style={styles.textoButton}>ENTRAR</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={()=>{}} style={styles.button}>
-        <Text style={styles.textoButton}>CADASTRAR</Text>
-      </TouchableOpacity>
+      <View style={{width:'80%'}}>
+        <View style={styles.containerTextTopInput}>
+          <Text style={styles.textTopInput}>
+            E-mail:
+          </Text>
+        </View>
+        <TextInput
+        style={styles.input}
+          onChangeText={setUserEmail}
+          value={userEmail}
+          placeholder="Inseira seu e-mail"
+          keyboardType="e
+          mail-address"
+        />
+        <Text style={[{display:errorEmail?'flex':'none'},styles.textTopInput]}>
+          E-mail deve ser no formato: exemplo@gmail.com
+        </Text>
+        <View style={styles.containerTextTopInput}>
+          <Text style={styles.textTopInput}>
+            Senha:
+          </Text>
+        </View>
+        <TextInput
+        style={styles.input}
+          onChangeText={setUserEmail}
+          value={userEmail}
+          placeholder="Inseira seu e-mail"
+          keyboardType="e
+          mail-address"
+        />
+        <Text style={[{display:errorPasseord?'flex':'none'},styles.textTopInput]}>
+          Senha deve ao menos ter 8 digitos, uma letra maiúscula, uma minúscula e um caractere especial
+        </Text>
+      </View>
+      
       <TouchableOpacity onPress={()=>{}} style={styles.buttonCall}>
         <Image source={require('./src/assets/call.png')} style={styles.icon} />
         <Text style={styles.textoButton}>193</Text>
@@ -39,11 +85,12 @@ const styles = StyleSheet.create({
   containerMain: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-around',
+    justifyContent: 'center',
     padding:20,
   },
   flexRow:{
     width:'100%',
+    height:80,
     flexDirection:'row',
     alignItems: 'center',
     justifyContent: 'space-around',
@@ -59,10 +106,11 @@ const styles = StyleSheet.create({
   },
   textBase:{
     fontSize:20,
-    color:'#64748b'
+    color:'#64748b',
+    fontWeight:'bold',
   },
   buttonSemiRounded:{
-    width:80,
+    width:10,
     height:50,
     borderRadius:'50%',
     alignItems:'center',
@@ -72,22 +120,28 @@ const styles = StyleSheet.create({
       width: 0,
       height: 0
     },
-    shadowOpacity:0.6,
-    shadowRadius:10
+    shadowOpacity:0.3,
+    shadowRadius:5
   },
-  andoridElevatoin:{
-    elevation:10
-  },
-  button:{
-    backgroundColor: '#fff',
-    borderWidth:1,
-    width:'40%',
-    margin:12,
-    padding:14,
-    borderRadius:8,
-    color: '#ff0000',
-    justifyContent: 'center',
+  containerTextTopInput:{
+    marginLeft:10, 
+    padding:5, 
+    transform: [{ translateY: 13 }],
+    backgroundColor:'#fff', 
+    zIndex:999, 
+    width:60, 
     alignItems:'center',
+    color: '#94a3b8',
+  },
+ textTopInput:{
+  color:'#94a3b8'
+ },
+  input:{
+    borderWidth:1,
+    borderColor:'#94a3b8',
+    borderRadius:4,
+    padding:10,
+    width:'100%',
   },
   textoButton:{
     fontSize:20,
@@ -104,9 +158,17 @@ const styles = StyleSheet.create({
     justifyContent:'center',
     width:150
   },
+  containerIcon:{
+    padding:10,
+    backgroundColor:'#fff',
+    borderRadius:'100%',
+    width:80,
+    alignItems:'center',
+    justifyContent:'center'
+  },
   icon:{
     width:40,
     height:40,
-    margin:5,
+    
   },
 });
